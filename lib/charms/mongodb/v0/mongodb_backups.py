b412c2ae3ee65c62d96794c7263eba66a8c02512
Mia Altieri <mgaltier200@gmail.com>
2023-01-24 16:39:54 +0000

    backup options set with s3 integrator

--- lib/charms/mongodb/v0/mongodb_backups.py
+++ lib/charms/mongodb/v0/mongodb_backups.py
@@ -13,8 +13,14 @@ import subprocess
 from charms.mongodb.v0.helpers import generate_password
 from charms.mongodb.v0.mongodb import MongoDBConfiguration
 from charms.operator_libs_linux.v1 import snap
+from ops.charm import RelationJoinedEvent
 from ops.framework import Object
 from ops.model import ActiveStatus, BlockedStatus
+from charms.data_platform_libs.v0.s3 import (
+    S3Requirer,
+    CredentialsGoneEvent,
+    CredentialsChangedEvent,
+)
 
 # The unique Charmhub library identifier, never change it
 LIBID = "18c461132b824ace91af0d7abe85f40e"
@@ -28,15 +34,14 @@ LIBPATCH = 1
 
 logger = logging.getLogger(__name__)
 
-
-PBM_S3_CONFIGS = [
-    ("storage.s3.region", "s3-storage-region"),
-    ("storage.s3.bucket", "s3-storage-bucket"),
-    ("storage.s3.prefix", "s3-storage-prefix"),
-    ("storage.s3.credentials.access-key-id", "s3-access-key-id"),
-    ("storage.s3.credentials.secret-access-key", "s3-secret-access-key"),
-    ("storage.s3.serverSideEncryption.kmsKeyID", "s3-kms-key-id"),
-]
+S3_PBM_OPTION_MAP = {
+    "region": "storage.s3.region",
+    "bucket": "storage.s3.bucket",
+    "path": "storage.s3.prefix",
+    "access-key": "storage.s3.credentials.access-key-id",
+    "secret-key": "storage.s3.credentials.secret-access-key",
+}
+S3_RELATION = "s3-credentials"
 
 
 class MongoDBBackups(Object):
@@ -47,10 +52,22 @@ class MongoDBBackups(Object):
         super().__init__(charm, "client-relations")
         self.charm = charm
         self.substrate = substrate
-        self.framework.observe(self.charm.on.config_changed, self._on_pbm_config_changed)
 
-    def _on_pbm_config_changed(self, event) -> None:
-        """Handles PBM configurations."""
+        # s3 relation handles the config options for s3 backups
+        self.s3_client = S3Requirer(self.charm, S3_RELATION)
+        # self.framework.observe(
+        #     self.charm.on[S3_RELATION].relation_joined, self._on_s3_relation_joined
+        # )
+        self.framework.observe(
+            self.s3_client.on.credentials_changed, self._on_s3_credential_changed
+        )
+        self.framework.observe(self.s3_client.on.credentials_gone, self._on_s3_credential_gone)
+
+    # def _on_s3_relation_joined(self, _: RelationJoinedEvent):
+    #     """On s3 credential relation joined."""
+    #     self.charm.unit.status = ActiveStatus()
+
+    def _on_s3_credential_changed(self, event: CredentialsChangedEvent):
         # handling PBM configurations requires that the pbm snap is installed.
         if "db_initialised" not in self.charm.app_peer_data:
             logger.debug("Cannot set PBM configurations, MongoDB has not yet started.")
@@ -73,10 +90,13 @@ class MongoDBBackups(Object):
         pbm_configs["storage.type"] = "s3"
         pbm_configs["storage.s3.serverSideEncryption.sseAlgorithm"] = "aws:kms"
 
-        # parse user configurations
-        for (snap_config_name, charm_config_name) in PBM_S3_CONFIGS:
-            if self.charm.config.get(charm_config_name):
-                pbm_configs[snap_config_name] = self.charm.config.get(charm_config_name)
+        credentials = self.s3_client.get_s3_connection_info()
+        for s3_option, s3_value in credentials.items():
+            if s3_option not in S3_PBM_OPTION_MAP:
+                continue
+
+            pbm_name = S3_PBM_OPTION_MAP[s3_option]
+            pbm_configs[pbm_name] = s3_value
 
         for (pbm_key, pbm_value) in pbm_configs.items():
             try:
@@ -92,6 +112,56 @@ class MongoDBBackups(Object):
                 return
 
         self.charm.unit.status = ActiveStatus("")
+
+        logger.info(f"First relation updated credentials: {credentials}")
+
+    def _on_s3_credential_gone(self, _: CredentialsGoneEvent):
+        logger.info("First s3 relation credentials GONE!")
+        # self.unit.status = WaitingStatus("Waiting for relation")
+
+    # def _on_pbm_config_changed(self, event) -> None:
+    #     """Handles PBM configurations."""
+    #     # handling PBM configurations requires that the pbm snap is installed.
+    #     if "db_initialised" not in self.charm.app_peer_data:
+    #         logger.debug("Cannot set PBM configurations, MongoDB has not yet started.")
+    #         event.defer()
+    #         return
+
+    #     snap_cache = snap.SnapCache()
+    #     pbm_snap = snap_cache["percona-backup-mongodb"]
+
+    #     if not pbm_snap.present:
+    #         logger.debug("Cannot set PBM configurations, PBM snap is not yet installed.")
+    #         event.defer()
+    #         return
+
+    #     # URI is set with `snap set`
+    #     pbm_snap.set({"uri": self._backup_config.uri})
+
+    #     # presets for PBM snap configurations
+    #     pbm_configs = {}
+    #     pbm_configs["storage.type"] = "s3"
+    #     pbm_configs["storage.s3.serverSideEncryption.sseAlgorithm"] = "aws:kms"
+
+    #     # parse user configurations
+    #     for (snap_config_name, charm_config_name) in PBM_S3_CONFIGS:
+    #         if self.charm.config.get(charm_config_name):
+    #             pbm_configs[snap_config_name] = self.charm.config.get(charm_config_name)
+
+    #     for (pbm_key, pbm_value) in pbm_configs.items():
+    #         try:
+    #             self._pbm_set_config(pbm_key, pbm_value)
+    #         except subprocess.CalledProcessError as e:
+    #             logger.error(
+    #                 "Failed to configure the PBM snap with key=value %s=%s, failed with error: %s",
+    #                 str(pbm_key),
+    #                 str(pbm_value),
+    #                 str(e),
+    #             )
+    #             self.charm.unit.status = BlockedStatus("couldn't configure s3 backup options.")
+    #             return
+
+    #     self.charm.unit.status = ActiveStatus("")
 
     def _pbm_set_config(self, key: str, value: str) -> None:
         """Runs the percona-backup-mongodb config command for the provided key and value."""
