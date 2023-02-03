4a4b7b99273bece602ca9b500255d867399dd8ba
Mia Altieri <mgaltier200@gmail.com>
2023-02-03 17:34:07 +0000

    minimise race conditions in test

--- tests/integration/backup_tests/helpers.py
+++ tests/integration/backup_tests/helpers.py
@@ -50,3 +50,18 @@ async def set_credentials(ops_test: OpsTest, cloud: str) -> None:
     parameters = {"access-key": access_key, "secret-key": secret_key}
     action = await s3_integrator_unit.run_action(action_name="sync-s3-credentials", **parameters)
     await action.wait()
+
+
+def is_relation_joined(ops_test: OpsTest, endpoint_one: str, endpoint_two: str) -> bool:
+    """Check if a relation is joined.
+
+    Args:
+        ops_test: The ops test object passed into every test case
+        endpoint_one: The first endpoint of the relation
+        endpoint_two: The second endpoint of the relation
+    """
+    for rel in ops_test.model.relations:
+        endpoints = [endpoint.name for endpoint in rel.endpoints]
+        if endpoint_one in endpoints and endpoint_two in endpoints:
+            return True
+    return False
