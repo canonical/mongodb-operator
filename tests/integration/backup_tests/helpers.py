3c010f39893559adae505b15659807d83b492339
Mia Altieri <32723809+MiaAltieri@users.noreply.github.com>
2023-02-10 02:36:25 -0700

    Backup int tests (#155)
    * backup list action
    
    * linting + fmt
    
    * resync twice in config
    
    * status reporting and validating
    
    * linter
    
    * remove unnecessary status setting
    
    * update pre-existing tests
    
    * Added basic integration tests
    
    * adding multiple bucket backups test
    
    * updating resync based on pbm bug
    
    * added stress test
    
    * use github secrets for s3
    
    * linting
    
    * reduce complexity
    
    * minor fixes to tests
    
    * update ci to include backup tests
    
    * adding s3-integrator charm
    
    * add GCP support
    
    * minimise race conditions in test
    
    * fix pbm error reporting, update s3 charm, fix credential passing
    
    * replacing s3 source code
    
    * Update lib/charms/mongodb/v0/mongodb_backups.py
    
    Co-authored-by: Mykola Marzhan <303592+delgod@users.noreply.github.com>
    
    * resolving PR comments
    
    * backup works with multiple replicas
    
    ---------
    
    Co-authored-by: Mykola Marzhan <303592+delgod@users.noreply.github.com>
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-41-145.eu-central-1.compute.internal>

--- tests/integration/backup_tests/helpers.py
+++ tests/integration/backup_tests/helpers.py
@@ -1,7 +1,11 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
+import os
 
+import ops
 from pytest_operator.plugin import OpsTest
+
+S3_APP_NAME = "s3-integrator"
 
 
 async def app_name(ops_test: OpsTest) -> str:
@@ -20,3 +24,44 @@ async def app_name(ops_test: OpsTest) -> str:
             return app
 
     return None
+
+
+async def count_logical_backups(db_unit: ops.model.Unit) -> int:
+    """Count the number of logical backups."""
+    action = await db_unit.run_action(action_name="list-backups")
+    list_result = await action.wait()
+    list_result = list_result.results["backups"]
+    list_result = list_result.split("\n")
+    backups = 0
+    for res in list_result:
+        backups += 1 if "logical" in res else 0
+
+    return backups
+
+
+async def set_credentials(ops_test: OpsTest, cloud: str) -> None:
+    """Sets the s3 crednetials for the provided cloud, valid options are AWS or GCP."""
+    # set access key and secret keys
+    access_key = os.environ.get(f"{cloud}_ACCESS_KEY", False)
+    secret_key = os.environ.get(f"{cloud}_SECRET_KEY", False)
+    assert access_key and secret_key, f"{cloud} access key and secret key not provided."
+
+    s3_integrator_unit = ops_test.model.applications[S3_APP_NAME].units[0]
+    parameters = {"access-key": access_key, "secret-key": secret_key}
+    action = await s3_integrator_unit.run_action(action_name="sync-s3-credentials", **parameters)
+    await action.wait()
+
+
+def is_relation_joined(ops_test: OpsTest, endpoint_one: str, endpoint_two: str) -> bool:
+    """Check if a relation is joined.
+
+    Args:
+        ops_test: The ops test object passed into every test case
+        endpoint_one: The first endpoint of the relation
+        endpoint_two: The second endpoint of the relation
+    """
+    for rel in ops_test.model.relations:
+        endpoints = [endpoint.name for endpoint in rel.endpoints]
+        if endpoint_one in endpoints and endpoint_two in endpoints:
+            return True
+    return False
