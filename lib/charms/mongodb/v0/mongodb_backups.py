0499aa5adfa89b69e9ceca35e19963183f7a7364
Mia Altieri <32723809+MiaAltieri@users.noreply.github.com>
2023-05-04 14:55:55 +0200

    bump libpatch

--- lib/charms/mongodb/v0/mongodb_backups.py
+++ lib/charms/mongodb/v0/mongodb_backups.py
@@ -43,7 +43,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 4
+LIBPATCH = 5
 MONGODB_SNAP_DATA_DIR = "/var/snap/charmed-mongodb/current"
 
 logger = logging.getLogger(__name__)
