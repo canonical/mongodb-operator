057b0b60109402b7ba835d16214f876d84b84d0d
Mia Altieri <32723809+MiaAltieri@users.noreply.github.com>
2023-05-17 11:25:40 +0200

    enable logging for mongod for k8s (#217)
    ## Issue
    By default the MongoDB K8s charm has its logs printed as the output of
    running the container and are not saved on the container. This means
    that the logs cannot be ported over to the cos bundle.
    
    
    ## Solution
    To enable logging we must change the start up arguments, these are
    generated within the shared library code and therefore the change
    library code change must be made in the VM charm first

--- tests/unit/test_mongodb_helpers.py
+++ tests/unit/test_mongodb_helpers.py
@@ -9,17 +9,14 @@ from charms.mongodb.v0.helpers import get_mongod_args
 
 class TestMongoDBHelpers(unittest.TestCase):
     def test_get_mongod_args(self):
-        service_args_auth = " ".join(
-            [
-                "--bind_ip_all",
-                "--replSet=my_repl_set",
-                "--dbpath=/var/snap/charmed-mongodb/common/var/lib/mongodb",
-                "--auth",
-                "--clusterAuthMode=keyFile",
-                "--keyFile=/var/snap/charmed-mongodb/current/etc/mongod/keyFile",
-                "\n",
-            ]
-        )
+        service_args_auth = [
+            "--bind_ip_all",
+            "--replSet=my_repl_set",
+            "--dbpath=/var/snap/charmed-mongodb/common/var/lib/mongodb",
+            "--auth",
+            "--clusterAuthMode=keyFile",
+            "--keyFile=/var/snap/charmed-mongodb/current/etc/mongod/keyFile",
+        ]
 
         config = mock.Mock()
         config.replset = "my_repl_set"
@@ -27,22 +24,33 @@ class TestMongoDBHelpers(unittest.TestCase):
         config.tls_internal = False
 
         self.assertEqual(
-            get_mongod_args(config, auth=True, snap_install=True),
+            get_mongod_args(config, auth=True, snap_install=True).split(),
             service_args_auth,
         )
 
-        service_args = " ".join(
-            [
-                # bind to localhost and external interfaces
-                "--bind_ip_all",
-                # part of replicaset
-                "--replSet=my_repl_set",
-                "--dbpath=/var/snap/charmed-mongodb/common/var/lib/mongodb",
-                "\n",
-            ]
-        )
+        service_args = [
+            # bind to localhost and external interfaces
+            "--bind_ip_all",
+            # part of replicaset
+            "--replSet=my_repl_set",
+            "--dbpath=/var/snap/charmed-mongodb/common/var/lib/mongodb",
+        ]
 
         self.assertEqual(
-            get_mongod_args(config, auth=False, snap_install=True),
+            get_mongod_args(config, auth=False, snap_install=True).split(),
+            service_args,
+        )
+
+        service_args = [
+            # bind to localhost and external interfaces
+            "--bind_ip_all",
+            # part of replicaset
+            "--replSet=my_repl_set",
+            "--dbpath=/var/lib/mongodb",
+            "--logpath=/var/lib/mongodb/mongodb.log",
+        ]
+
+        self.assertEqual(
+            get_mongod_args(config, auth=False, snap_install=False).split(),
             service_args,
         )
