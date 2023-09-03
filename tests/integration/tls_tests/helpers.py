dee1282fba9d17884e48c70be2a5f1e93211de08
Dmitry Ratushnyy <dmitry.ratushnyy@canonica.com>
2023-09-03 07:35:50 +0000

    Fix tls tests

--- tests/integration/tls_tests/helpers.py
+++ tests/integration/tls_tests/helpers.py
@@ -2,6 +2,7 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 from datetime import datetime
+from importlib.metadata import version
 
 import ops
 from pytest_operator.plugin import OpsTest
