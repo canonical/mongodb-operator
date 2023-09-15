9b64f319b4be0d2ece66889d22422a7afbb36d13
Mia Altieri <mgaltier200@gmail.com>
2023-09-15 08:09:37 +0000

    use correct snap

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ class Config:
     MONGODB_SNAP_DATA_DIR = "/var/snap/charmed-mongodb/current"
     MONGOD_CONF_DIR = f"{MONGODB_SNAP_DATA_DIR}/etc/mongod"
     MONGOD_CONF_FILE_PATH = f"{MONGOD_CONF_DIR}/mongod.conf"
-    SNAP_PACKAGES = [("charmed-mongodb", "5/edge/mongos", 83)]
+    SNAP_PACKAGES = [("charmed-mongodb", "5/edge", 84)]
     CONFIG_SERVER = "config-server"
     REPLICATION = "replication"
     SHARD = "shard"
