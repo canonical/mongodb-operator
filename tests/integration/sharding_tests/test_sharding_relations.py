8d00835951244ce220a7137315862a2212a4096c
Mia Altieri <mgaltier200@gmail.com>
2023-11-01 17:28:15 +0000

    add legacy tests

--- tests/integration/sharding_tests/test_sharding_relations.py
+++ tests/integration/sharding_tests/test_sharding_relations.py
@@ -2,17 +2,19 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 import pytest
-from pytest_operator.plugin import OpsTest
 from juju.errors import JujuAPIError
+from pytest_operator.plugin import OpsTest
 
 SHARD_ONE_APP_NAME = "shard-one"
 CONFIG_SERVER_ONE_APP_NAME = "config-server-one"
 CONFIG_SERVER_TWO_APP_NAME = "config-server-two"
 APP_CHARM_NAME = "application"
+LEGACY_APP_CHARM_NAME = "legacy-application"
 
 CONFIG_SERVER_REL_NAME = "config-server"
 SHARD_REL_NAME = "sharding"
 DATABASE_REL_NAME = "first-database"
+LEGACY_RELATION_NAME = "obsolete"
 
 # for now we have a large timeout due to the slow drainage of the `config.system.sessions`
 # collection. More info here:
@@ -21,7 +23,9 @@ TIMEOUT = 30 * 60
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest, application_charm, database_charm) -> None:
+async def test_build_and_deploy(
+    ops_test: OpsTest, application_charm, legacy_charm, database_charm
+) -> None:
     """Build and deploy a sharded cluster."""
     await ops_test.model.deploy(
         database_charm,
@@ -37,18 +41,18 @@ async def test_build_and_deploy(ops_test: OpsTest, application_charm, database_c
         database_charm, num_units=1, config={"role": "shard"}, application_name=SHARD_ONE_APP_NAME
     )
     await ops_test.model.deploy(application_charm, application_name=APP_CHARM_NAME)
+    await ops_test.model.deploy(legacy_charm, application_name=LEGACY_APP_CHARM_NAME)
 
-    async with ops_test.fast_forward():
-        await ops_test.model.wait_for_idle(
-            apps=[
-                CONFIG_SERVER_ONE_APP_NAME,
-                CONFIG_SERVER_TWO_APP_NAME,
-                SHARD_ONE_APP_NAME,
-            ],
-            idle_period=20,
-            raise_on_blocked=False,
-            timeout=TIMEOUT,
-        )
+    await ops_test.model.wait_for_idle(
+        apps=[
+            CONFIG_SERVER_ONE_APP_NAME,
+            CONFIG_SERVER_TWO_APP_NAME,
+            SHARD_ONE_APP_NAME,
+        ],
+        idle_period=20,
+        raise_on_blocked=False,
+        timeout=TIMEOUT,
+    )
 
 
 async def test_only_one_config_server_relation(ops_test: OpsTest) -> None:
@@ -78,7 +82,58 @@ async def test_cannot_use_db_relation(ops_test: OpsTest) -> None:
         SHARD_ONE_APP_NAME,
     )
 
+    await ops_test.model.wait_for_idle(
+        apps=[SHARD_ONE_APP_NAME],
+        idle_period=20,
+        raise_on_blocked=False,
+        timeout=TIMEOUT,
+    )
+
     shard_unit = ops_test.model.applications[SHARD_ONE_APP_NAME].units[0]
     assert (
         shard_unit.workload_status_message == "Sharding roles do not support database interface."
     ), "Shard cannot be related using the database relation"
+
+    await ops_test.model.applications[SHARD_ONE_APP_NAME].remove_relation(
+        f"{APP_CHARM_NAME}:{DATABASE_REL_NAME}",
+        SHARD_ONE_APP_NAME,
+    )
+
+    await ops_test.model.wait_for_idle(
+        apps=[SHARD_ONE_APP_NAME],
+        idle_period=20,
+        raise_on_blocked=False,
+        timeout=TIMEOUT,
+    )
+
+
+async def test_cannot_use_legacy_db_relation(ops_test: OpsTest) -> None:
+    """Verify that a shard cannot use the DB relation."""
+    await ops_test.model.integrate(
+        LEGACY_APP_CHARM_NAME,
+        SHARD_ONE_APP_NAME,
+    )
+
+    await ops_test.model.wait_for_idle(
+        apps=[SHARD_ONE_APP_NAME],
+        idle_period=20,
+        raise_on_blocked=False,
+        timeout=TIMEOUT,
+    )
+
+    shard_unit = ops_test.model.applications[SHARD_ONE_APP_NAME].units[0]
+    assert (
+        shard_unit.workload_status_message == "Sharding roles do not support obsolete interface."
+    ), "Shard cannot be related using the mongodb relation"
+
+    await ops_test.model.applications[SHARD_ONE_APP_NAME].remove_relation(
+        f"{SHARD_ONE_APP_NAME}:{LEGACY_RELATION_NAME}",
+        f"{LEGACY_APP_CHARM_NAME}:{LEGACY_RELATION_NAME}",
+    )
+
+    await ops_test.model.wait_for_idle(
+        apps=[SHARD_ONE_APP_NAME],
+        idle_period=20,
+        raise_on_blocked=False,
+        timeout=TIMEOUT,
+    )
