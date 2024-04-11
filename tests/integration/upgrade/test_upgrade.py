56af82a49c0b66a8128d5243de3437b954fdc030
MiaAltieri <mgaltier200@gmail.com>
2024-04-11 15:26:30 +0000

    restructure tests

--- tests/integration/upgrade/test_upgrade.py
+++ tests/integration/upgrade/test_upgrade.py
@@ -7,8 +7,8 @@ import logging
 import pytest
 from pytest_operator.plugin import OpsTest
 
-from ..helpers import check_or_scale_app, find_unit, get_app_name
 from ..ha_tests import helpers as ha_helpers
+from ..helpers import check_or_scale_app, find_unit, get_app_name
 
 logger = logging.getLogger(__name__)
 
@@ -40,8 +40,8 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
 
 
 @pytest.mark.skip(reason="Must wait for stable channel to support upgrades before testing")
-async def test_preflight_check(ops_test: OpsTest) -> None:
-    """Verifies that the preflight check can run successfully."""
+async def test_upgrade(ops_test: OpsTest, continuous_writes) -> None:
+    """Verifies that the upgrade can run successfully."""
     app_name = await get_app_name(ops_test)
     leader_unit = await find_unit(ops_test, leader=True, app_name=app_name)
     logger.info("Calling pre-upgrade-check")
@@ -52,10 +52,6 @@ async def test_preflight_check(ops_test: OpsTest) -> None:
         apps=[app_name], status="active", timeout=1000, idle_period=120
     )
 
-
-@pytest.mark.skip(reason="Must wait for stable channel to support upgrades before testing")
-async def test_upgrade(ops_test: OpsTest, continuous_writes) -> None:
-    """Verifies that the upgrade can run successfully."""
     new_charm = await ops_test.build_charm(".")
     app_name = await get_app_name(ops_test)
     await ops_test.model.applications[app_name].refresh(path=new_charm)
@@ -67,3 +63,17 @@ async def test_upgrade(ops_test: OpsTest, continuous_writes) -> None:
     total_expected_writes = await ha_helpers.stop_continous_writes(ops_test, app_name=app_name)
     actual_writes = await ha_helpers.count_writes(ops_test, app_name=app_name)
     assert total_expected_writes["number"] == actual_writes
+
+
+@pytest.mark.skip(reason="Must wait for stable channel to support upgrades before testing")
+async def test_preflight_check(ops_test: OpsTest) -> None:
+    """Verifies that the preflight check can run successfully."""
+    app_name = await get_app_name(ops_test)
+    leader_unit = await find_unit(ops_test, leader=True, app_name=app_name)
+    logger.info("Calling pre-upgrade-check")
+    action = await leader_unit.run_action("pre-upgrade-check")
+    await action.wait()
+
+    await ops_test.model.wait_for_idle(
+        apps=[app_name], status="active", timeout=1000, idle_period=120
+    )
