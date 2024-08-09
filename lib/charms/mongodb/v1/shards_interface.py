597d3f1238a37387e8967a1b72ee5f097c6da826
Neha Oudin <neha@oudin.red>
2024-08-09 11:05:55 +0200

    fix: lib versions

--- lib/charms/mongodb/v1/shards_interface.py
+++ lib/charms/mongodb/v1/shards_interface.py
@@ -63,7 +63,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 10
+LIBPATCH = 8
 
 KEYFILE_KEY = "key-file"
 HOSTS_KEY = "host"
