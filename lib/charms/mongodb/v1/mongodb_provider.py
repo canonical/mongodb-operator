86069e329e1bb22cc81888f40a53f4fdf98cab4d
Neha Oudin <neha@oudin.red>
2024-10-04 14:48:38 +0200

    chore: Grant enableSharding permission to admin and default

--- lib/charms/mongodb/v1/mongodb_provider.py
+++ lib/charms/mongodb/v1/mongodb_provider.py
@@ -31,7 +31,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 13
+LIBPATCH = 14
 
 logger = logging.getLogger(__name__)
 REL_NAME = "database"
@@ -506,6 +506,7 @@ class MongoDBProvider(Object):
     def _get_roles_from_relation(relation: Relation) -> Set[str]:
         """Return additional user roles from relation if specified or return None."""
         roles = relation.data[relation.app].get("extra-user-roles", None)
+        logger.error(f"ROLES = {roles}")
         if roles is not None:
             return set(roles.split(","))
         return {"default"}
