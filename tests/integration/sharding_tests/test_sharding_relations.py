28f058e9d9587821e0c57f25bb74991d7e9f5bcf
Neha Oudin <neha@oudin.red>
2025-06-12 14:56:07 +0200

    fix: sharding  relation test

--- tests/integration/sharding_tests/test_sharding_relations.py
+++ tests/integration/sharding_tests/test_sharding_relations.py
@@ -264,7 +264,7 @@ async def test_shard_mongos_relation(ops_test: OpsTest) -> None:
     await wait_for_mongodb_units_blocked(
         ops_test,
         SHARD_ONE_APP_NAME,
-        status="Relation to mongos not supported, config role must be config-server",
+        status="Relation to mongos not supported",
         timeout=300,
     )
 
