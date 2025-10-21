f216613f2f6309e0cd035e28b0e0a06c3de6445d
Neha Oudin <neha@oudin.red>
2025-10-21 19:14:17 +0200

    fix: remove group

--- tests/integration/test_sharding.py
+++ tests/integration/test_sharding.py
@@ -30,7 +30,6 @@ CONFIG_SERVER_REL_NAME = "config-server"
 TIMEOUT = 30 * 60
 
 
-@pytest.mark.runner(["self-hosted", "linux", "X64", "noble", "large"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest) -> None:
@@ -81,7 +80,6 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
     await wait_for_mongodb_units_blocked(ops_test, SHARD_THREE_APP_NAME, timeout=300)
 
 
-@pytest.mark.runner(["self-hosted", "linux", "X64", "noble", "large"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_cluster_active(ops_test: OpsTest) -> None:
