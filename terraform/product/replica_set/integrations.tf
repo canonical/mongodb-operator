8d5294e09f17caee811fa63cd225251b07933ef3
Neha Oudin <17551419+Gu1nness@users.noreply.github.com>
2025-11-20 17:55:15 +0100

    chore-(DPE-8728): Use TF Provider 1.0 (#592)

--- terraform/product/replica_set/integrations.tf
+++ terraform/product/replica_set/integrations.tf
@@ -8,7 +8,6 @@
 ## Same model integrations
 
 resource "juju_integration" "mongodb_grafana_agent_integration" {
-  model = var.mongodb.model
   application {
     name = var.mongodb.app_name
   }
@@ -19,14 +18,14 @@ resource "juju_integration" "mongodb_grafana_agent_integration" {
     module.mongodb,
     juju_application.grafana_agent,
   ]
+  model_uuid = var.mongodb.model_uuid
 }
 
 resource "juju_integration" "mongodb_tls_same_model_integration" {
-  for_each = local.enable_tls && var.self_signed_certificates.model == var.mongodb.model ? { "integrated" = true } : {}
+  for_each = local.enable_tls && var.self_signed_certificates.model_uuid == var.mongodb.model_uuid ? { "integrated" = true } : {}
 
-  model = var.mongodb.model
   application {
-    name = var.mongodb.app_name
+    name     = var.mongodb.app_name
     endpoint = "certificates"
   }
   application {
@@ -36,12 +35,12 @@ resource "juju_integration" "mongodb_tls_same_model_integration" {
     module.mongodb,
     juju_application.self-signed-certificates["deployed"],
   ]
+  model_uuid = var.mongodb.model_uuid
 }
 
 resource "juju_integration" "mongodb_s3_same_model_integration" {
-  for_each = var.s3_integrator.model == var.mongodb.model ? { "integrated" = true } : {}
+  for_each = var.s3_integrator.model_uuid == var.mongodb.model_uuid ? { "integrated" = true } : {}
 
-  model = var.mongodb.model
   application {
     name = var.mongodb.app_name
   }
@@ -52,11 +51,11 @@ resource "juju_integration" "mongodb_s3_same_model_integration" {
     module.mongodb,
     juju_application.s3_integrator,
   ]
+  model_uuid = var.mongodb.model_uuid
 }
 
 resource "juju_integration" "mongodb_data_same_model_integration" {
-  for_each = var.data_integrator.model == var.mongodb.model ? { "integrated" = true } : {}
-  model    = var.mongodb.model
+  for_each = var.data_integrator.model_uuid == var.mongodb.model_uuid ? { "integrated" = true } : {}
 
   application {
     name = var.mongodb.app_name
@@ -68,12 +67,12 @@ resource "juju_integration" "mongodb_data_same_model_integration" {
     module.mongodb,
     juju_application.data_integrator,
   ]
+  model_uuid = var.mongodb.model_uuid
 }
 
 ## Cross model integrations
 resource "juju_integration" "mongodb_data_cross_model_integration" {
-  for_each = var.data_integrator.model != var.mongodb.model ? { "integrated" = true } : {}
-  model    = var.data_integrator.model
+  for_each = var.data_integrator.model_uuid != var.mongodb.model_uuid ? { "integrated" = true } : {}
 
   application {
     offer_url = juju_offer.mongodb_client_offer["offered"].url
@@ -86,11 +85,11 @@ resource "juju_integration" "mongodb_data_cross_model_integration" {
     juju_application.data_integrator,
     juju_offer.mongodb_client_offer,
   ]
+  model_uuid = var.data_integrator.model_uuid
 }
 
 resource "juju_integration" "mongodb_tls_cross_model_integration" {
-  for_each = local.enable_tls && var.self_signed_certificates.model != var.mongodb.model ? { "integrated" = true } : {}
-  model    = var.mongodb.model
+  for_each = local.enable_tls && var.self_signed_certificates.model_uuid != var.mongodb.model_uuid ? { "integrated" = true } : {}
 
   application {
     offer_url = juju_offer.tls_provider_offer["offered"].url
@@ -103,11 +102,11 @@ resource "juju_integration" "mongodb_tls_cross_model_integration" {
     module.mongodb,
     juju_offer.tls_provider_offer,
   ]
+  model_uuid = var.mongodb.model_uuid
 }
 
 resource "juju_integration" "mongodb_s3_cross_model_integration" {
-  for_each = var.s3_integrator.model != var.mongodb.model ? { "integrated" = true } : {}
-  model    = var.mongodb.model
+  for_each = var.s3_integrator.model_uuid != var.mongodb.model_uuid ? { "integrated" = true } : {}
 
   application {
     offer_url = juju_offer.s3_integrator_offer["offered"].url
@@ -120,4 +119,5 @@ resource "juju_integration" "mongodb_s3_cross_model_integration" {
     module.mongodb,
     juju_offer.s3_integrator_offer,
   ]
+  model_uuid = var.mongodb.model_uuid
 }
